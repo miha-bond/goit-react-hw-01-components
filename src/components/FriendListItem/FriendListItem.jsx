770d021d87e8fced82dc21c56ceb46e07b18076f
Miha Bondarenko <limon18@ukr.net>
2022-11-10 10:42:56 +0200

    add task3

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import styles from './FriendListItem.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => (
-  <li className="item">
+  <li className={styles.item}>
     <span className={isOnline ? styles.online : styles.offline}></span>
     <img className={styles.avatar} src={avatar} alt={name} width="48" />
     <p className={styles.name}>{name}</p>
